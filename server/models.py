d00edd6b47b76e46eea541cd8b3f928437ba597c
Emmanuel <zacmac807@gmail.com>
2024-07-03 21:02:58 +0300

    complete task

--- server/models.py
+++ server/models.py
@@ -1,52 +1,3 @@
-# from flask_sqlalchemy import SQLAlchemy
-# from sqlalchemy import MetaData
-# from sqlalchemy.ext.associationproxy import association_proxy
-# from sqlalchemy_serializer import SerializerMixin
-
-
-# metadata = MetaData(naming_convention={
-#     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
-# })
-
-# db = SQLAlchemy(metadata=metadata)
-
-
-# class Customer(db.Model, SerializerMixin):
-#     __tablename__ = 'customers'
-
-#     id = db.Column(db.Integer, primary_key=True)
-#     name = db.Column(db.String)
-#     reviews = db.relationship('Review', back_populates='customer')
-
-#     def __repr__(self):
-#         return f'<Customer {self.id}, {self.name}>'
-
-
-# class Item(db.Model, SerializerMixin):
-#     __tablename__ = 'items'
-
-#     id = db.Column(db.Integer, primary_key=True)
-#     name = db.Column(db.String)
-#     price = db.Column(db.Float)
-#     reviews = db.relationship('Review', back_populates='item')
-
-#     def __repr__(self):
-#         return f'<Item {self.id}, {self.name}, {self.price}>'
-
-
-# class Review(db.Model,, SerializerMixin):
-#     __tablename__ = 'items'
-#     id = db.Column(db.Integer, primary_key=True)
-#     comment = db.Column(db.String)
-#     customer_id = db.Column(db.Integer, db.ForeignKey('customers.id'))
-#     item_id = db.Column(db.Integer, db.ForeignKey('items.id'))
-#     customer = db.relationship('Customer', back_populates ='reviews')
-#     item = db.relationship('Item', back_populates ='reviews')
-
-#     def __repr__(self):
-#         return f'<Review {self.id}, {self.comment}>'
-
-
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
 from sqlalchemy.ext.associationproxy import association_proxy
@@ -112,3 +63,5 @@ class Review(db.Model, SerializerMixin):
 
     def __repr__(self):
         return f"<Review {self.id}, {self.comment}, {self.customer.name}, {self.item.name}>"
+
+
